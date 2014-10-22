d5d20d28eadcd95cdad3971fbf909bb649c97e16
specialshe <specialshe65@gmail.com>
2014-10-22 12:23:56 +0900

    Revert "This is a program to change .xml to .http"
    This reverts commit 9d744cdf819400017e64fdb38ce709d5d15f13eb.

--- utils_private/glog_label_remover.py
+++
@@ -1,9 +0,0 @@
-import re
-
-rr = re.compile(r"^[IWE]\d+\s+\d+:\d+:\d+\.\d+\s+\d+\s+\w+\.(cpp|hpp):\d+\]")
-#r"[IWE]\d+\s\d+:\d+:\d+\.\d+\s\s\d+\s\w+\.(hpp|cpp):\d+\]"
-import sys
-
-for line in sys.stdin:
-  sys.stdout.write(rr.sub("", line))
-
--- utils_private/glog_timestamp_remover.py
+++
@@ -1,9 +0,0 @@
-import re
-
-rr = re.compile(r"[IWE]\d+\s+\d+:\d+:\d+\.\d+\s+\d+\s+")
-
-import sys
-
-for line in sys.stdin:
-  sys.stdout.write(rr.sub("", line))
-
