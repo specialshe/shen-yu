dca8a6ed0674e6b1a97b0c896bcd8c968464fc9d
specialshe <specialshe65@gmail.com>
2014-10-23 12:30:31 +0900

    change some algorithme to modify

--- utils_private/instrictipon.pl
+++ utils_private/instrictipon.pl
@@ -2,32 +2,50 @@ use strict;
 use utf8;
 use Data::Dumper;
 open(FILE,"<:encoding(utf8)","bitext_m_3188.xml");
+open(FILE2,">:encoding(utf8)","bitext_m_3188a.xml");
 binmode(STDIN, ':encoding(utf8)'); 
 binmode(STDOUT, ':encoding(utf8)');
 binmode(STDERR, ':encoding(utf8)'); 
 my $needlength=2;
 my $line;
 my @mark1;
+#a tempo hash for check
 my @mark2;
+# a tempo hash for check
 my @tree1;
+# an array to store data structure of language i. ->[0] is his parent. ->[1] is his hinsi(verb,nous). ->[2] is is genkei (shita->suru)
 my @tree2;
+# an array to store data structure of language j.
 my $length1;
+#length of tree1
 my $length2;
+#length of tree2
 my @align1;
+#alignment,$align1[$i] aligns to $align2[$i]
 my @align2;
+#alignment,$align1[$i] aligns to $align2[$i]
 my $lengthalign;
+#length of align
 my $i;
 my $j;
 my $k;
 my $mark1;
 my @tongji;
+#the number of extractable treelets
 my @cuowu;
+# an array to store unextractable treelets
 my @align3;
+my @statealign;
+#an array to store which parsing is better then another . 0 is language i, 1 is language j.
 my @where1;
+#a node i's index in align1
 my @where2;
+#a node j's index in align2
 my @document1;
+#just store raw data in .xml to write back
 while ($line=<FILE>)
 {
+  #read .xml files for initial 
   push(@document1,$line);
   if ($line=~ /<para_sentence.*>/)
   {
@@ -40,8 +58,11 @@ while ($line=<FILE>)
 	@mark2=();
 	@tongji=();
 	@cuowu=();
+	@statealign=();
+	@where1=();
+	@where2=();
 	$j=0;
-	<STDIN>;
+	#<STDIN>;
   }
   if ($line=~ /<i_data>/)
   {
@@ -116,6 +137,7 @@ while ($line=<FILE>)
 	    $mark2[$j]=0;
 	  }
    }
+   # calculate @where
    for $i (0..$lengthalign)
    {
      for $j (@{$align1[$i]})
@@ -162,8 +184,22 @@ while ($line=<FILE>)
 	   }
 	 }
    }
+   #calculate @statealign
+   for $i (0..$lengthalign)
+   {
+     $statealign[$i]=&state($i);
+   }
+  #for $i (0..$length1)
+   #{
+   #  print $i,"=",$statealign[$where1[$i]],"\n";
+  # }
+  #print Dumper @statealign;
+  #<STDIN>;
+
     #print Dumper \@where1;
 	#print Dumper \@mark2;
+	
+	#get all unextractable Treelets;
 	for $i (0..$lengthalign)
 	{
 	  for $j (@{$align1[$i]})
@@ -187,13 +223,15 @@ while ($line=<FILE>)
 	  pop(@align3);
 	}
 	print "\n";
+	#for all the errors,repair it.
 	for $i (@cuowu)
 	{
 	  my $mark3;
 	  @mark1=();
-	  $mark3=&xiangxin($i);
-	  &writeback();
+	  $mark3=&xiangxin($i);	  
 	}
+	#writeback, to create a new .xml file
+	&writeback();
 	for $i (1..$needlength)
 	{
 	  print $tongji[$i]->[0]," ",$tongji[$i]->[1],"\n";
@@ -202,12 +240,36 @@ while ($line=<FILE>)
 	#last;
   }
 }
+close (FILE2);
+sub state()
+{
+  my $i=shift;
+  my $flag7;
+  $i=$align1[$i]->[0];
+  $i=$tree1[$i]->[0];
+  while (($i ne -1) && ($tree1[$i+1]->[2] ne "、/、") && ($tree1[$i]->[2] ne "と/と") && ($tree1[$i]->[2] ne "も/も") && ($tree1[$i]->[2] ne "に/に"))
+  {
+    if ($tree1[$i]->[1]=~ /動詞/) 
+	{
+	  $flag7=$flag7+1;
+	}
+	$i=$tree1[$i]->[0];
+  }
+  if ($flag7>=2) 
+  {
+    return 1;
+  }
+  else 
+  {
+    return 0;
+  }
+}
 sub writeback()
 {
   my $line;
   my $mark1;
   my $i;
-  open(FILE2,">:encoding(utf8)","1.xml");
+  $document1[0]=~ s/id="(.+?)"/id="$1a"/;
   for $line (@document1)
   {
     if ($line=~ /<i_data>/)
@@ -234,7 +296,6 @@ sub writeback()
   }
   print FILE2 $line;
  }
- close (FILE2);
 }
 sub xiangxin()
 {
@@ -270,7 +331,7 @@ sub xiangxin()
 	}
   }
   if ($flag6 ne 1) {$flag4=0;}
-  print Dumper @mark1;
+  #print Dumper @mark1;
   while (($tree1[$flag5]->[2] ne "と/と") && ($tree1[$flag5]->[2] ne "も/も") && ($tree1[$flag5]->[2] ne "に/に") &&($flag5 ne "-1") && ($tree1[$flag5+1]->[2] ne "、/、"))
   {
     if ($tree1[$flag5]->[1] =~ /動詞/)
@@ -282,18 +343,23 @@ sub xiangxin()
   }
   print $flag5,"\n";
   print $flag4,"\n";
- <STDIN>;
-  if ($flag4<2)
-  {
+  print $document1[0];
+ #<STDIN>;
     my $temp1;
     for $i (@{$flag3})
 	{
+	  if ($statealign[$i] eq 0)
+	  {
 	  my $mark4=0;
 	  for $j (@{$align1[$i]})
 	  {
 	    if ($where1[$j] ne $where1[$tree1[$j]->[0]])
 		{
 		  $temp1=$where1[$tree1[$j]->[0]];
+		}
+		if ($temp1 eq "")
+		{
+		  $mark4=1;
 		}
 		if ($tree1[$j]->[0] eq "-1")
 		{
@@ -319,20 +385,19 @@ sub xiangxin()
 		}
 	  }
 	  }
-	}
-  }
-  else
-  {
-    my $temp1;
-	
-    for $i (@{$flag3})
-	{
+	  }
+	  if ($statealign[$i] eq 1)
+	  {
 	  my $mark4=0;
 	  for $j (@{$align2[$i]})
 	  {
 	     if ($where2[$j] ne $where2[$tree2[$j]->[0]])
 		 {
 		   $temp1=$where2[$tree2[$j]->[0]];
+		 }
+		 if ($temp1 eq "")
+		 {
+		   $mark4=1;
 		 }
 		 if ($tree2[$j]->[0] eq "-1")
 		 {
@@ -356,8 +421,8 @@ sub xiangxin()
 		}
 	   }
 	  }
+	  }
 	}
-  }
 }
 sub lays2()
 {
